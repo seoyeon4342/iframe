d1aeb423e6508edaf63e1eb81b6e930fc344df7a
seoyeon4342 <seoyeonh212@naver.com>
2024-08-08 15:05:11 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 
+    const header = document.querySelector("#top header");
     const navBtn = document.querySelector("header button");
     const nav = document.querySelector("header nav");
     const iframe = window.parent.document.getElementById("header");
 
 
+
+    let i = 0;
     const navSlide = () => {
-        iframe.classList.add("slide");
+        if(i == 0){
+            iframe.classList.add("slide");
+        navBtn.textContent = "close";
+        i++;
+        } else navHide();
+        
+    }
+
+    const navHide = () => {
+        iframe.classList.remove("slide");
+        navBtn.textContent = "menu";
+        i = 0;
     }
 
 
     navBtn.addEventListener("click", navSlide);
+    header.addEventListener("click", navHide);
 
 
 
